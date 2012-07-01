77bb15ec1e89deac1801144fe92b8dabf02ee324
Andreas Eger <dev@eger-andreas.de>
2012-07-01 02:33:22 +0200

    new aliases
    - quick ssh to my raspberry in my local lan
    - latexmk with xelatex

--- .aliases.zsh
+++ .aliases.zsh
@@ -14,3 +14,8 @@ alias be='bundle exec '
 
 alias gvim="gvim --remote-tab-silent "
 alias gim="gvim "
+
+alias pi="ssh sch1zo@192.168.178.25 -t tmux"
+
+alias xelatexmk='latexmk -pvc -e "\$pdflatex=q/xelatex %O -interaction=nonstopmode -synctex=1 %S/" -pdf '
+alias pdflatexmk='latexmk -pvc -e "\$pdflatex=q/pdflatex %O -interaction=nonstopmode -synctex=1 %S/" -pdf '
