980a88b940c47536456e6c6d03d8fe8f4680008e
Andreas Eger <dev@eger-andreas.de>
2013-07-11 11:08:49 +0200

    skip lucene ramdisk check

--- .config/fish/functions.fry/pjpp.fish
+++ .config/fish/functions.fry/pjpp.fish
@@ -1,8 +1,8 @@
 function pjpp
   set -l lucene_base /usr/experteer/var
-  if not test -f $lucene_base/lucene/index.jobs/_h.cfx
+  if not test -f $lucene_base/lucene/index.jobs/_x.cfx
     echo "lucene doesn't exists, lets put it in the ramdisk..."
-    sudo chown aeger:users $lucene_base/lucene -R; and cp $lucene_base/plucene/* $lucene_base/lucene/ -R
+    #sudo chown aeger:users $lucene_base/lucene -R; and cp $lucene_base/plucene/* $lucene_base/lucene/ -R
   end
 
   set -l pjpp_base $HOME/ram
