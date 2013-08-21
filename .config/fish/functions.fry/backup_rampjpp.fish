e1086f75decbe2e89328e822cbd595dc1d011fa9
Andreas Eger <dev@eger-andreas.de>
2013-08-21 15:47:04 +0200

    improved ramdisk backup

--- .config/fish/functions.fry/backup_rampjpp.fish
+++ .config/fish/functions.fry/backup_rampjpp.fish
@@ -1,3 +1,7 @@
 function backup_rampjpp
-  rsync -va --exclude=log/\* --delete $HOME/ram/pjpp/ $HOME/code/pjpp
+  for x in pjpp pjpp32
+    if test -e $HOME/ram/$x/CURRENT_INSTALL
+      rsync -va --exclude-from=$HOME/pjpp_sync_excludes --delete $HOME/ram/$x/ $HOME/code/$x
+    end
+  end
 end
--- .config/fish/functions.fry/pjpp.fish
+++ .config/fish/functions.fry/pjpp.fish
@@ -8,7 +8,7 @@ function pjpp
   set -l pjpp_base $HOME/ram
   if not test -f $pjpp_base/pjpp/CURRENT_INSTALL
     echo "pjpp not ready, copying to ramdisk"
-    sudo chown aeger:users $pjpp_base -R; and rsync -av --stats --exclude=/log/\* $HOME/code/pjpp  $pjpp_base
+    sudo chown aeger:users $pjpp_base -R; and rsync -av --stats --exclude-from=$HOME/pjpp_sync_excludes $HOME/code/pjpp/  $pjpp_base/pjpp
   end
 
   cd $pjpp_base/pjpp; and pjpp_server.rb
