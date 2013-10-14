b1e0dc4a3828d5cd99d2d19e404714043ea82fef
Andreas Eger <dev@eger-andreas.de>
2013-10-14 11:47:51 +0200

    disable vimpager

--- .config/fish/load/env.fish
+++ .config/fish/load/env.fish
@@ -5,9 +5,9 @@ setenv LANGUAGE en_US.UTF-8
 setenv LANG en_US.UTF-8
 setenv LC_ALL en_US.UTF-8
 
-#setenv PAGER less
-#setenv LESS "-iMSx4 -R"
+setenv PAGER less
+setenv LESS "-iMSx4 -R"
 
-setenv PAGER vimpager
+# setenv PAGER vimpager
 alias less $PAGER
 alias zless $PAGER
