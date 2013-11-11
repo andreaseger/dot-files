7f203e9bdf17c5729e1fde1a2ef95e691962cc1e
Andreas Eger <dev@eger-andreas.de>
2013-11-11 10:26:17 +0100

    update redshift night temp, add BUNDLE_GEMFILE helper

---
+++ .config/fish/functions.fry/set_local_gemfile.fish
@@ -0,0 +1,4 @@
+function set_local_gemfile
+  setenv BUNDLE_GEMFILE 'Gemfile.local'
+  echo "using 'Gemfile.local'"
+end
---
+++ .config/fish/functions.fry/unset_local_gemfile.fish
@@ -0,0 +1,3 @@
+function unset_local_gemfile
+  set --erase BUNDLE_GEMFILE
+end
