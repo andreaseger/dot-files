696879705c4b44b50a88392fd592baff8ddf6fe1
Andreas Eger <dev@eger-andreas.de>
2013-11-22 16:34:28 +0100

    use abbreviation instead of functions for rails helper

--- .config/fish/config.fry.fish
+++ .config/fish/config.fry.fish
@@ -1,3 +1,5 @@
 setenv __local_identities $HOME/.ssh/experteer_rsa
 load-ssh-agent
 
+abbreviate 'server=bundle exec rails s'
+abbreviate 'console=bundle exec pry -r config/environment'
--- .config/fish/functions.fry/console.fish
+++
@@ -1,3 +0,0 @@
-function console --description "start rails console in pry"
-  command bundle exec pry -r config/environment
-end
