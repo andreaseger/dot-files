fd35dbb5cd83b3a4bae50a4d3d64da91eb7ecb26
Andreas Eger <dev@eger-andreas.de>
2013-12-05 23:15:21 +0100

    update snapshot_and_update helper

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,6 +1,6 @@
 function update
   if test -x ~/.local/bin/snapshot_and_update
-    command sudo snapshot_and_update
+    command snapshot_and_update
   else
     sudo powerpill -Syu; and pacaur -Syu
   end
