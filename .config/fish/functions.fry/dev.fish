0b2721423fe638f05993810b9c7efad3449813a0
Andreas Eger <dev@eger-andreas.de>
2013-12-11 11:02:29 +0100

    update basic ssh config

--- .config/fish/functions.fry/dev.fish
+++ .config/fish/functions.fry/dev.fish
@@ -1,4 +1,3 @@
 function dev
-  ssh -A -t dev.experteer.com -- /home/aeger/.local/bin/fish
-  # ssh -t aeger@dev.experteer.com -- screen -URD
+  command ssh -t dev.experteer.com -- /home/aeger/.local/bin/fish
 end
