854d60c3d888f05756ee5e55d87dfa69f49140f9
Andreas Eger <dev@eger-andreas.de>
2013-12-16 09:49:55 +0100

    remove pjpp32

--- .config/fish/functions.fry/pjpp.fish
+++ .config/fish/functions.fry/pjpp.fish
@@ -1,15 +1,9 @@
 function pjpp
-#  set -l lucene_base /usr/experteer/var
-#  if not test -f $lucene_base/lucene/index.jobs/_x.cfx
-#    echo "lucene doesn't exists, lets put it in the ramdisk..."
-#    #sudo chown aeger:users $lucene_base/lucene -R; and cp $lucene_base/plucene/* $lucene_base/lucene/ -R
-#  end
-
   set -l pjpp_base $HOME/ram
   if not test -f $pjpp_base/pjpp/CURRENT_INSTALL
     echo "pjpp not ready, copying to ramdisk"
     sudo chown aeger:users $pjpp_base -R; and rsync -av --stats --exclude-from=$HOME/pjpp_sync_excludes $HOME/code/pjpp/  $pjpp_base/pjpp
   end
 
-  cd $pjpp_base/pjpp; and pjpp_server.rb
+  cd $pjpp_base/pjpp
 end
--- .config/fish/functions.fry/pjpp32.fish
+++
@@ -1,9 +0,0 @@
-function pjpp32
-  set -l pjpp_base $HOME/ram
-  if not test -f $pjpp_base/pjpp32/CURRENT_INSTALL
-    echo "pjpp not ready, copying to ramdisk"
-    sudo chown aeger:users $pjpp_base -R; and rsync -av --stats --exclude-from=$HOME/pjpp_sync_excludes $HOME/code/pjpp32/ $pjpp_base/pjpp32
-  end
-
-  cd $pjpp_base/pjpp32
-end
