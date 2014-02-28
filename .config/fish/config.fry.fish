9d8f4ddf302d97c6d6cb78035bfdae39a2298fe9
Andreas Eger <dev@eger-andreas.de>
2014-02-28 15:24:59 +0100

    fix config.fry.fish

--- .config/fish/config.fry.fish
+++ .config/fish/config.fry.fish
@@ -2,6 +2,7 @@ setenv __local_identities $HOME/.ssh/experteer_rsa
 load-ssh-agent
 
 abbreviate 'server=bundle exec rails server'
-abbreviate 'console=bundle exec rails console
+abbreviate 'console=bundle exec rails console'
 
 . $fish_path/load/rvm.fish
+
