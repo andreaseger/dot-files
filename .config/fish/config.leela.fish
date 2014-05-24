259228c93a728f14c901d3cfdc7a0d4ff5eb7a91
Andreas Eger <dev@eger-andreas.de>
2014-05-24 17:04:20 +0200

    update config for ubuntu on leela

--- .config/fish/config.leela.fish
+++ .config/fish/config.leela.fish
@@ -1,7 +1,7 @@
 #setenv __local_identities $HOME/.ssh/experteer_rsa
 load-ssh-agent
 
-set -U fish_user_paths $fish_user_paths '/usr/local/heroku/bin'
+#set -U fish_user_paths $fish_user_paths '/usr/local/heroku/bin'
 . $fish_path/load/rbenv.fish
 . $fish_path/load/virtual.fish
 
