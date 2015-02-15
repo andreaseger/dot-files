f30ed2787e0c1e8c171eb87de2de0392866a6ac6
Andreas Eger <dev@eger-andreas.de>
2015-02-15 21:54:22 +0000

    add nvim abbreviates for cubert

--- .config/fish/config.bender.fish
+++ .config/fish/config.bender.fish
@@ -1,5 +1,3 @@
-set fish_user_paths $fish_user_paths /usr/local/bin/
-
 . $fish_path/load/rbenv.fish
 
 abbreviate 'vi=nvim'
--- .config/fish/config.cubert.fish
+++ .config/fish/config.cubert.fish
@@ -1,1 +1,4 @@
 . $fish_path/load/rbenv.fish
+
+abbreviate 'vi=nvim'
+abbreviate 'vim=nvim'
