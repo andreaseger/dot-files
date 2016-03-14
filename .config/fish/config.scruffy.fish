911776e737a0a027255efc603dd0426e6e160912
Andreas Eger <andreas.eger@runtastic.com>
2016-03-14 08:23:20 +0100

    update scruffy config

--- .config/fish/config.scruffy.fish
+++ .config/fish/config.scruffy.fish
@@ -3,4 +3,4 @@ set -x JRUBY_OPTS "--dev -J-noverify"
 . $fish_path/load/rbenv.fish
 
 abbr -a rweb='cd ~/runtastic/runtastic-web'
-abbr -a pr='env RBENV_VERSION=ruby-2.2.3 stash pull-request -o (current_branch) master'
+abbr -a pr='env RBENV_VERSION=ruby-2.2.4 stash pull-request -o (current_branch) master'
