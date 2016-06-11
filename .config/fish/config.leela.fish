f8cbad7d58795bd220e7c6874eb3cb5951982b27
Andreas Eger <dev@eger-andreas.de>
2016-06-11 15:38:14 +0200

    add jruby opts

--- .config/fish/config.leela.fish
+++ .config/fish/config.leela.fish
@@ -1,5 +1,6 @@
 #setenv __local_identities $HOME/.ssh/experteer_rsa
 load-ssh-agent
+set -x JRUBY_OPTS "--dev -J-noverify"
 
 #set -U fish_user_paths $fish_user_paths '/usr/local/heroku/bin'
 . $fish_path/load/rbenv.fish
