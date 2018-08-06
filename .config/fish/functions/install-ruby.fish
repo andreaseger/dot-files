e527a501e88518c868300c987f2c62533f8b1a78
Andreas Eger <dev@eger-andreas.de>
2018-08-06 14:12:21 +0200

    update configs

--- .config/fish/functions/install-ruby.fish
+++ .config/fish/functions/install-ruby.fish
@@ -1,9 +1,9 @@
 function install-ruby --wraps=ruby-install -a "type" -a "ruby_version"
   echo "installing $type $ruby_version"
   if test (which rbenv)
-    command ruby-install --rubies-dir ~/.rbenv/versions $argv -- --disable-install-rdoc
+    command ruby-install --rubies-dir ~/.rbenv/versions $argv --cleanup --jobs -- --disable-install-rdoc
   else if test (which rvm)
-    command ruby-install --rubies-dir ~/.rvm/rubies $argv -- --disable-install-rdoc
+    command ruby-install --rubies-dir ~/.rvm/rubies $argv --cleanup --jobs -- --disable-install-rdoc
   else
     command ruby-install $argv -- --disable-install-rdoc
   end
