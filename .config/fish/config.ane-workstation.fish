3960cd10d626bc01205336349821831e69d451e4
Andreas Eger <dev@eger-andreas.de>
2019-05-30 15:49:38 +0200

    tmux updates, git lfs

--- .config/fish/config.ane-workstation.fish
+++ .config/fish/config.ane-workstation.fish
@@ -2,4 +2,5 @@
 . $fish_path/load/rbenv.fish
 
 set -x JRUBY_OPTS "--dev -J-noverify"
-abbr -a pr 'env RBENV_VERSION=ruby-2.5.3 stash pull-request -o (current_branch) master'
+abbr -a pr 'env RBENV_VERSION=ruby-2.5.3 stash pull-request (current_branch) master'
+add_uniquely_to_user_paths $HOME/.cargo/bin
