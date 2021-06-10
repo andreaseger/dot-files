8ac4c33ee6fed0ae05d0b2950beaa7ff5b9cdc71
Andreas Eger <dev@eger-andreas.de>
2021-06-10 17:47:56 +0200

    changes

--- .config/fish/functions/install-ruby.fish
+++ .config/fish/functions/install-ruby.fish
@@ -1,7 +1,7 @@
 function install-ruby --wraps=ruby-install -a "type" -a "ruby_version"
   if test (which rbenv)
     echo "installing $type $ruby_version"
-    command ruby-install --rubies-dir ~/.rbenv/versions $argv --no-install-deps --cleanup --jobs
+    command ruby-install --rubies-dir ~/.rbenv/versions $argv --no-install-deps --cleanup --jobs 4
     set -x RBENV_VERSION $type-$ruby_version
     echo "-- post install steps --"
     command ruby -v
