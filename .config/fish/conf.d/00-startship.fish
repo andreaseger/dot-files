efc6107b61aa2e6f1e4ff2fc9db6d1e7b1a45ba1
Andreas Eger <andreaseger@users.noreply.github.com>
2023-04-30 19:59:04 +0200

    update prompt

--- .config/fish/conf.d/00-startship.fish
+++ .config/fish/conf.d/00-startship.fish
@@ -1,10 +1,10 @@
-# starship config
-# function starship_transient_prompt_func
-#   starship module character
-# end
-# function starship_transient_rprompt_func
-#   starship module time
-# end
+#  starship config
+function starship_transient_prompt_func
+  starship module character
+end
+function starship_transient_rprompt_func
+  starship module time
+end
 
 starship init fish | source
-# enable_transience
+#enable_transience
