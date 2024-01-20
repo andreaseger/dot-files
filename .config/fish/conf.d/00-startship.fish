0ebac879c1c3dfcca451957d6f264282b050b3e6
Andreas Eger <andreaseger@users.noreply.github.com>
2024-01-20 15:34:13 +0100

    auto-install starship and add

--- .config/fish/conf.d/00-startship.fish
+++ .config/fish/conf.d/00-startship.fish
@@ -1,4 +1,9 @@
 #  starship config
+if not type -q starship
+  echo "no starship, lets auto-install it to ~/.local/bin"
+  curl -sS https://starship.rs/install.sh | env BIN_DIR=$HOME/.local/bin env FORCE=true sh
+end
+
 function starship_transient_prompt_func
   starship module character
 end
