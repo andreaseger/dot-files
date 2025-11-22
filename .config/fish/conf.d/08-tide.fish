ce2a349ce3bc3cb49d55bc09f40d32d2f946df2f
Andreas Eger <andreaseger@users.noreply.github.com>
2025-11-22 20:52:54 +0100

    fix dotf alias and tide setup

--- .config/fish/conf.d/08-tide.fish
+++ .config/fish/conf.d/08-tide.fish
@@ -16,17 +16,20 @@ if not test -e "$__fish_config_dir/corals/$coral"
             echo "get something from here https://github.com/ryanoasis/nerd-fonts"
     end
     # set theme (in case we have other plugins with a fish_prompt function)
-    reef theme plttn/tide
+    reef theme $coral
     # load tide for current session
     source (path resolve $__fish_config_dir/corals/$coral/conf.d/*.fish)
 end
 
 if not set -q tide_configured
     echo "Configuring Tide prompt..."
-    tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Darkest --show_time='24-hour format' --classic_prompt_separators=Vertical --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Many icons' --transient=Yes
-    
+    tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Darkest --show_time='24-hour format' --classic_prompt_separators=Vertical --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Many icons'
+
     set -U tide_configured true
 end
 
+# Enable transient prompt by default
+set -g fish_transient_prompt 1
+
 # TBD replace all nerdfont icons with suitable alternatives
-# see .config/fish/corals/IlanCosman/tide/functions/tide/configure/icons.fish
+# see .config/fish/corals/plttn/tide/functions/tide/configure/icons.fish
--- .config/fish/functions/__utilities/dotf.fish
+++ .config/fish/functions/__utilities/dotf.fish
@@ -1,3 +1,3 @@
 function dotf --wraps git --description 'Manage dotfiles with bare git repo'
-    command git --git-dir=$HOME/.local/share/dot-files/ --work-tree=$HOME $argv
+    command git --git-dir=$HOME/.local/share/dot-files.git/ --work-tree=$HOME $argv
 end
