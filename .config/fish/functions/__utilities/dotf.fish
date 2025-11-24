beee9d02ad4ea64c89067ec1a636249a6c7643d1
Andreas Eger <andreaseger@users.noreply.github.com>
2025-11-24 11:40:05 +0100

    add some more dotf helpers

--- .config/fish/functions/__utilities/dotf.fish
+++ .config/fish/functions/__utilities/dotf.fish
@@ -1,3 +1,12 @@
 function dotf --wraps git --description 'Manage dotfiles with bare git repo'
     command git --git-dir=$HOME/.local/share/dot-files.git/ --work-tree=$HOME $argv
 end
+
+function dotf-up
+    dotf fetch
+    dotf rebase --autostash --rebase-merges m
+end
+
+function dotf-push
+    dotf push -u origin m
+end
