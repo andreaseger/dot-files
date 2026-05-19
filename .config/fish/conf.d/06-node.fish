ef102306306751e0a99edc79c9f357951f618a3a
Andreas Eger <andreaseger@users.noreply.github.com>
2026-05-19 10:34:32 +0200

    add pnpm bin

--- .config/fish/conf.d/06-node.fish
+++ .config/fish/conf.d/06-node.fish
@@ -2,4 +2,8 @@ if test -d "$HOME/.bun/bin"
     fish_add_path -U "$HOME/.bun/bin"
 end
 
-alias nx="npx nx"
+#pnpm
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+    set -gx PATH "$PNPM_HOME/bin" $PATH
+end
